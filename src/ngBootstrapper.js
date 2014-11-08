1ec8b23942d651775b3619c9c0f6b7b6ee14ef20
Mark Polak <mark@thedutchies.com>
2014-11-08 15:31:10 +0100

    Add comments to the bootstrapper

--- src/ngBootstrapper.js
+++ src/ngBootstrapper.js
@@ -1,16 +1,19 @@
 (function (window, angular) {
-    function NgBootstrapper(appName, element) { // jshint: ignore
+
+    /**
+     * Constructs a new instance of the bootstrapper
+     *
+     * @param {String} appName Name of the app module that needs to be bootstrapped
+     * @param {DomNode} element The element that the module needs to be attached to
+     * @constructor
+     */
+    function NgBootstrapper(appName, element) {
         if (!appName || typeof appName !== 'string') {
             throw 'App name should be a string';
         }
 
         //Will throw an error if the module does not exist
         angular.module(appName);
-
-        window.name = 'NG_DEFER_BOOTSTRAP!';
-        angular.bootstrap(element, [appName]);
-        //Reset the bootstrapping defer
-        window.name = undefined;
 
         this.appName = appName;
         this.remoteInjectableConfigs = [];
@@ -20,8 +23,25 @@
 
         //Create a new injector for this bootstrapper
         this.injector = angular.injector(window.getBootstrapper.$inject);
+
+        //Defer the bootstrapping by setting the name flag
+        window.name = 'NG_DEFER_BOOTSTRAP!';
+
+        angular.bootstrap(element, [this.appName]);
+
+        //Reset the bootstrapping defer
+        window.name = undefined;
     }
 
+    /**
+     * Specify a remote source by setting the name and the location.
+     * The souces will be loaded when the bootstrap method is called.
+     *
+     * @param {String} name The name this data will be known by, the injectable name
+     * @param {String} url The url where the data is located
+     *
+     * @returns {NgBootstrapper} Returns itself for chaining purposes
+     */
     NgBootstrapper.prototype.addInjectableFromRemoteLocation = function (name, url) {
         if (typeof name !== 'string') {
             throw 'name should be a string';
@@ -39,6 +59,11 @@
         return this;
     };
 
+    /**
+     * Execute the ajax calls for the dependencies that are set using one of the add methods
+     *
+     * @returns {Array} Returns an array of promises that represent the requests
+     */
     NgBootstrapper.prototype.executeRemoteCalls = function () {
         var promises = [];
         var $http = this.injector.get('$http');
@@ -60,6 +85,13 @@
         return promises;
     };
 
+    /**
+     * Creates the factories on the module that will be injected with the external results.
+     * It adds a factory to the module that returns the data on the response.
+     * It looks for a data property on each of the responses.
+     *
+     * @param {Array} responses Array of responses that have been recieved from the ajax calls.
+     */
     NgBootstrapper.prototype.createFactories = function (responses) {
         var module = this.module;
         angular.forEach(responses, function (response) {
@@ -69,6 +101,12 @@
         });
     };
 
+    /**
+     * Perform the actual bootstrapping. It executes the remote calls and when all of
+     * them are successful will create the injectables and bootstrap the app.
+     *
+     * @returns {NgBootstrapper} Returns itself for chaining purposes
+     */
     NgBootstrapper.prototype.bootstrap = function () {
         var self = this;
         var $q = this.injector.get('$q');
@@ -85,6 +123,13 @@
         return this;
     };
 
+    /**
+     * Exposed method on the window to get a new bootstrapper.
+     *
+     * @param {String} appName Name of the app module that needs to be bootstrapped.
+     * @param {DomNode} element Dom element that the app will be attached to.
+     * @returns {NgBootstrapper} Returns the instance of the bootstrapper.
+     */
     window.getBootstrapper = function (appName, element) {
         //Defer the bootstrapping of any apps
 
