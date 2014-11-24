72eee6bc8907c417f2a7df1287f3c5120193a43b
pbarnette <paul.barnette@us.pwc.com>
2014-11-24 09:10:31 -0500

    Added the userfilter service

---
+++ src/userlist/userfilter-service.js
@@ -0,0 +1,24 @@
+angular.module('PEPFAR.usermanagement').service('userFilterService', userFilterService);
+
+function userFilterService($q) {
+    var deferred = $q.defer();
+    var userFilter = [
+        { name: 'Name' },
+        {name: 'Username'},
+        {name: 'E-Mail'},
+        {name: 'Roles'},
+        {name: 'User Groups'},
+        {name: 'Organisation Unit'},
+        {name: 'Types'}
+    ];
+
+    deferred.resolve(userFilter);
+
+    return {
+        getUserFilter: getUserFilter
+    };
+
+    function getUserFilter() {
+        return deferred.promise;
+    }
+}
