55d9b3824ffb6d404b1bffce5e29eb9797d5eef8
pbarnette <paul.barnette@us.pwc.com>
2014-11-24 19:48:03 -0500

    Connecting to the endpoint, adding the typeahead functionality

--- src/userlist/userlist-controller.js
+++ src/userlist/userlist-controller.js
@@ -1,6 +1,6 @@
 angular.module('PEPFAR.usermanagement').controller('userListController', userListController);
 
-function userListController($scope, userFilter, userTypes, dataGroups) {
+function userListController($scope, userFilter, userTypes, dataGroups, Restangular, errorHandler) {
     var vm = this;
 
     //$scope.userTypes = userTypes || [];
@@ -32,4 +32,37 @@ function userListController($scope, userFilter, userTypes, dataGroups) {
         }
 
     };
+
+    $scope.getUserList = function (val) {
+        var inputText = val;
+        window.console.log(inputText);
+
+        return Restangular.one('users.json').get({
+            fields: 'id,name',
+            filter: getFilterOptions(inputText),
+            paging: false
+        }).then(function (response) {
+            window.console.log(response);
+            /*
+            var inputMatch = response.userRoles;
+
+            actions.forEach(function (action) {
+                //Only search roles for type independent actions
+                if (action.typeDependent) { return true; }
+
+                action.userRoleId = userRoles.reduce(function (current, value) {
+                    if (value.name === action.userRole) {
+                        return value.id;
+                    }
+                    return current;
+                }, action.userRoleId);
+            });
+*/
+        }, errorHandler.errorFn('Failed to load user roles for actions'));
+    };
+
+    function getFilterOptions(inputText) {
+        inputText = '';
+        return inputText;
+    }
 }
