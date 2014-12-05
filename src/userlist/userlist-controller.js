c22578e41f988860257b136b3d6fed1400b8e588
pbarnette <paul.barnette@us.pwc.com>
2014-12-05 13:23:57 -0500

    Made updates for text, changed the placeholder based on selection

--- src/userlist/userlist-controller.js
+++ src/userlist/userlist-controller.js
@@ -1,6 +1,6 @@
 angular.module('PEPFAR.usermanagement').controller('userListController', userListController);
 
-function userListController(userFilter, userTypes, dataGroupsService, userListService, userStatusService, errorHandler) { //jshint ignore:line
+function userListController($scope, userFilter, userTypes, dataGroupsService, userListService, userStatusService, errorHandler) { //jshint ignore:line
     var vm = this;
 
     vm.detailsOpen = false;
@@ -21,6 +21,7 @@ function userListController(userFilter, userTypes, dataGroupsService, userListSe
     vm.detailsUserUserType = '';
     vm.getUserType = getUserType;
     vm.doSearch = doSearch;
+    vm.placeHolder = 'Search for user';
 
     vm.search = {
         options: userFilter,
@@ -84,10 +85,7 @@ function userListController(userFilter, userTypes, dataGroupsService, userListSe
     }
 
     function isProcessing(id) {
-        if (vm.processing[id] && vm.processing[id] === true) {
-            return true;
-        }
-        return false;
+        return (vm.processing[id] && vm.processing[id] === true) ? true : false;
     }
 
     function showDetails(user) {
@@ -115,6 +113,29 @@ function userListController(userFilter, userTypes, dataGroupsService, userListSe
                 errorHandler.warning('Failed to load datagroups for user');
             });
     }
+
+    //function placeHolder() {
+    //   var currentFilter = vm.search.filterType.name.toLowerCase();
+    //    var phText = 'Search for';
+    //    window.console.log(phText + ' ' + currentFilter);
+    //}
+
+    $scope.$watch('userList.search.filterType', function (newVal) {
+        var phText = 'Search for ';
+        var outputStr = '';
+
+        if (newVal) {
+
+            if (newVal.name === 'E-Mail' || newVal.name === 'Roles') {
+                outputStr = phText + 'user ';
+            } else {
+                outputStr = phText;
+            }
+            //vm.placeHolder = phText + ' ' + newVal.name;
+
+            vm.placeHolder = outputStr + newVal.name;
+        }
+    });
 
     //TODO: Move this out and perhaps use the available usertype services
     function getUserType(user) {
@@ -178,80 +199,4 @@ function userListController(userFilter, userTypes, dataGroupsService, userListSe
         loadList();
     }
 
-//    //$scope.userTypes = userTypes || [];
-//    $scope.userFilter = userFilter;
-//    $scope.filter = $scope.userFilter[0];
-//    $scope.filterRefine = [];
-//    //vm.userFilter = userFilterService.getUserFilter();
-//
-//    vm.userTypes = userTypes;
-//    vm.dataGroups = dataGroups;
-//
-//    vm.users = [
-//        {name: 'Mark'},
-//        {name: 'Paul'}
-//    ];
-//
-//    $scope.setFilterRefine = function () {
-//        window.console.log($scope.filter.name);
-//        $scope.filterRefine = [];
-//
-//        if ($scope.filter.name === 'Types') {
-//            $scope.filterRefine = vm.userTypes;
-//            $scope.filterSecond = vm.userTypes[0];
-//        } else if ($scope.filter.name === 'User Groups') {
-//            $scope.filterRefine = vm.dataGroups;
-//            $scope.filterSecond = vm.dataGroups[0];
-//        }
-//
-//    };
-//
-//    $scope.getUserList = function (val) {
-//        var inputText = val;
-//        window.console.log(inputText);
-//
-//        return Restangular.one('users.json').get({
-//            fields: 'id,name,email,userGroups,userCredentials',
-//            filter: getFilterOptions(inputText),
-//            paging: true,
-//            pageSize: 25
-//        }).then(function (response) {
-//            vm.users = [];
-//            return response.users.map(function (item) {
-//                vm.users.push(item);
-//                return item.name;
-//            });
-//
-///*
-//            return response.data.users.map(function (item) {
-//                return item.name;
-//            });
-//*/
-//            /*
-//            var inputMatch = response.userRoles;
-//
-//            actions.forEach(function (action) {
-//                //Only search roles for type independent actions
-//                if (action.typeDependent) { return true; }
-//
-//                action.userRoleId = userRoles.reduce(function (current, value) {
-//                    if (value.name === action.userRole) {
-//                        return value.id;
-//                    }
-//                    return current;
-//                }, action.userRoleId);
-//            });
-//*/
-//        }, errorHandler.errorFn('Failed to load user roles for actions'));
-//    };
-//
-//    function getFilterOptions(inputText) {
-//        window.console.log('Filter Name: %o    Second Filter: %o  Text Input: %o',
-//            $scope.filter, $scope.filterSecond, inputText);
-//        //window.console.log($scope.filterRefine);
-//        inputText = '';
-//        return inputText;
-//    }
-
-    //function buildFilterOption
 }
